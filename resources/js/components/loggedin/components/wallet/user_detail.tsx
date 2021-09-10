57bf26962d14bb381e3fa65e4087a66d3b525a7e
aksamlwanga <lwangaaksam@gmail.com>
2021-09-10 03:39:57 +0300

    hello

---
+++ resources/js/components/loggedin/components/wallet/user_detail.tsx
@@ -0,0 +1,18 @@
+export class  UserPayment {
+    firstname: string='Aksam' ;
+    lastname: string='Lwanga' ;
+    address1: string ='Kayunga';
+    address2: string='' ;
+    state: string ='town';
+    country: string ='Uganda';
+    code: string='2192';
+    city: string='Kayunga' ;
+    allow:boolean=true;
+    cardname: string='';
+    cardno: number=0;
+    date: Date = new Date();
+    cvv: number=0;
+    amount:number=0;
+ 
+
+}
