ea8ec836ada081337f32be40244afdf8c0b04ec9
hellomukama <xandarcyber@gmail.com>
2021-09-11 04:17:49 +0300

    Seeding #1

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,17 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    /** Run the database seeds. */
     public function run()
     {
-        //
+        User::factory()->count(15)->create()
+            ->each(function ($user) {
+                $user->save();
+            });
     }
 }
