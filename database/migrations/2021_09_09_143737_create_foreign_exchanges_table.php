149fa6c6192e3cc6fdd09b114eac46cafda9586e
hellomukama <xandarcyber@gmail.com>
2021-09-11 05:38:16 +0300

    Altered datatype on foreign_exchanges table

--- database/migrations/2021_09_09_143737_create_foreign_exchanges_table.php
+++ database/migrations/2021_09_09_143737_create_foreign_exchanges_table.php
@@ -13,8 +13,8 @@ class CreateForeignExchangesTable extends Migration
             $table->id();
             $table->unsignedBigInteger('forex_id');
             $table->foreign('forex_id')->references('id')->on('countries')->onDelete('cascade');
-            $table->unsignedInteger('source_currency');
-            $table->unsignedInteger('destination_currency');
+            $table->string('source_currency');
+            $table->string('destination_currency');
             $table->unsignedInteger('rate');
             $table->string('country');
             $table->timestamps();   // updated_at and created_at are very applicable on this one.
@@ -26,4 +26,15 @@ class CreateForeignExchangesTable extends Migration
     {
         Schema::dropIfExists('foreign_exchanges');
     }
+    
+    // TODO
+    //There will be a need to set the sizes of the individual fields
+
+
+    // TODO
+    // Create or rather restructure the relationships in a way that;
+    // If 'A' holds a PK, and 'B', has a FK to A, and 'C' has a relationship with 'B'
+    // Laravel understands that 'C' is automatically related to A and therefore there shouldn't be a direct
+    // definition of a relationship between 'C' & 'A'
+
 }
